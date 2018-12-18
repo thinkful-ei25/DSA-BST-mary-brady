d272dd54af3f403558103d3961463a3b662f398b
mkcnly <mkcnly@gmail.com>
2018-12-18 11:52:38 -0800

    added 2nd drill

--- bst.js
+++ bst.js
@@ -5,7 +5,7 @@ Create a Binary Search Tree called BST and insert 3, 1, 4, 6, 9, 2, 5, 7 to your
 */
 
 class BST {
-  constructor(key=null, value=null, parent=null) {
+  constructor(key = null, value = null, parent = null) {
     this.key = key;
     this.value = value;
     this.parent = parent;
@@ -14,50 +14,95 @@ class BST {
   }
 
   insert(key, value) {
-    if(this.key == null) {
+    if (this.key == null) {
       this.key = key;
       this.value = value;
-    } else if(key < this.key) {
-      if(this.left == null) {
+    } else if (key < this.key) {
+      if (this.left == null) {
         this.left = new BST(key, value, this);
       } else {
         this.left.insert(key, value);
       }
     } else {
-      if(this.right == null) {
+      if (this.right == null) {
         this.right = new BST(key, value, this);
       } else {
-        this.right.insert(key,value);
+        this.right.insert(key, value);
       }
     }
   }
 
   find(key) {
-    if(this.key == key) {
+    if (this.key == key) {
       return this.value;
-    } else if(key < this.key && this.left) {
+    } else if (key < this.key && this.left) {
       return this.left.find(key);
-    } else if(key > this.key && this.right) {
-        return this.right.find(key);
+    } else if (key > this.key && this.right) {
+      return this.right.find(key);
     } else {
       throw new Error('Key Error: find() - Key was not found');
     }
-  } 
+  }
 
-  remove(key){
-    if(this.key == key) {
-      if(this.left && this.right) {
+  remove(key) {
+    if (this.key == key) {
+      if (this.left && this.right) {
+        const successor = this.right._findMin();
+        this.key = successor.key;
+        this.value = successor.value;
+        successor.remove(successor.key);
+      }
+      else if (this.left) {
+        this._replaceWith(this.left);
+      }
+      else if (this.right) {
+        this._replaceWith(this.right);
+      }
+      else {
+        this._replaceWith(null);
+      }
+    }
+    else if (key < this.key && this.left) {
+      this.left.remove(key);
+    }
+    else if (key > this.key && this.right) {
+      this.right.remove(key);
+    }
+    else {
+      throw new Error('Key error: remove() Was not successful');
+    }
+  }
 
+  _replaceWith(node) {
+    if (this.parent) {
+      if (this == this.parent.left) {
+        this.parent.left = node;
+      }
+      else if (this == this.parent.right) {
+        this.parent.right = node;
+      }
+      if (node) {
+        node.parent = this.parent;
+      }
+    }
+    else {
+      if (node) {
+        this.key = node.key;
+        this.value = node.value;
+        this.left = node.left;
+        this.right = node.right;
+      }
+      else {
+        this.key = null;
+        this.value = null;
+        this.left = null;
+        this.right = null;
       }
     }
   }
 
-  _replaceWith(node){
-
-  }
-
-  _findMin(){
-    if(!this.left){
+  _findMin() {
+    if (!this.left) {
       return this;
     }
     return this.left._findMin();
--- drills.js
+++ drills.js
@@ -1,5 +1,14 @@
 const BST = require('./bst');
 
+function heightOf(node) {
+  if (node == null) {
+    return -1;
+  }
+  else {
+    return 1 + Math.max(heightOf(node.left), heightOf(node.right));
+  }
+}
+
 function main() {
   let searchTree = new BST();
   searchTree.insert(3);
@@ -11,6 +20,27 @@ function main() {
   searchTree.insert(5);
   searchTree.insert(7);
   console.log(searchTree);
+
+  console.log(heightOf(searchTree))
 }
+
+// 3,1,4,6,9,2,5,
+//SearchTree:
+//       3
+//     /   \
+//     1    4
+//     \    \
+//      2    6
+//          / \
+//          5  9
+
+//input st
+//if null return -1
+//have count
+//else return 1+ Math.Max(heightofBSTfunction(node.left, node.right));
+//go down one side and save variable for count on left
+//go down the right side and save variable for count on right
+//height is left count or right count dependant on which is larger
+
 
 main();
